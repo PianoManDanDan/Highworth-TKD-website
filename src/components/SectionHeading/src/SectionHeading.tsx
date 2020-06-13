14965cc3c1f33052e7651f7695bf4417be085877
Daniel Martin <Daniel.Martin@valtech.com>
2020-06-13 14:10:43 +0100

    Add SectionHeading component

--- src/components/SectionHeading/src/SectionHeading.tsx
+++ src/components/SectionHeading/src/SectionHeading.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { SectionHeadingProps } from './types';
 import './SectionHeading.scss';
 
-export const SectionHeading: React.FC<SectionHeadingProps> = ({ text, id }) => {
-	return (
-		<div id={id} className="section-heading">
-			{text}
-		</div>
-	);
+export const SectionHeading: React.FC<SectionHeadingProps> = ({ text }) => {
+	return <div className="section-heading">{text}</div>;
 };
--- src/components/SectionHeading/src/types/SectionHeadingTypes.ts
+++ src/components/SectionHeading/src/types/SectionHeadingTypes.ts
@@ -1,4 +1,3 @@
 export type SectionHeadingProps = {
 	text: string;
-	id: string;
 };
