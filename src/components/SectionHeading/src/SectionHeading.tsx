cbbfa4deab646def1f281df9e40c29ce5ef2c16b
Daniel Martin <djm_djm@hotmail.com>
2020-06-16 01:00:38 +0100

    Add aria level and role to section headings

--- src/components/SectionHeading/src/SectionHeading.tsx
+++ src/components/SectionHeading/src/SectionHeading.tsx
@@ -3,5 +3,9 @@ import { SectionHeadingProps } from './types';
 import './SectionHeading.scss';
 
 export const SectionHeading: React.FC<SectionHeadingProps> = ({ text }) => {
-	return <div className="section-heading">{text}</div>;
+	return (
+		<div className="section-heading" role="heading" aria-level={1}>
+			{text}
+		</div>
+	);
 };
