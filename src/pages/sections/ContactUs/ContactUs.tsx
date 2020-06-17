1c8a07109dac1a5b782053ecb65580139e193715
Daniel Martin <djm_djm@hotmail.com>
2020-06-17 23:49:59 +0100

    Remove fortawesome dependency in favour of react-icons

--- src/pages/sections/ContactUs/ContactUs.tsx
+++ src/pages/sections/ContactUs/ContactUs.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
 import { SectionHeading } from '../../../components/SectionHeading/src';
 import './ContactUs.scss';
 
@@ -11,9 +12,9 @@ export const ContactUs: React.FC<{}> = () => {
 			<div className="section-body">
 				You can contact our head instructor using the following links:
 				<br />
-				<FontAwesomeIcon icon={faPhoneAlt} className="font-awesome-icon" />
+				<FaPhoneAlt className="font-awesome-icon" />
 				Call us on <a href="tel:+447795271155">07795 271155</a>.<br />
-				<FontAwesomeIcon icon={faEnvelope} className="font-awesome-icon" />
+				<FaEnvelope className="font-awesome-icon" />
 				Email us at{' '}
 				<a href="mailto:alisonclack@hotmail.co.uk">alisonclack@hotmail.co.uk</a>
 				.
